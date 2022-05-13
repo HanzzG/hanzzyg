419506faa4d1c956d08cfa4f88655bfb055ad39b
Hanzz <aliyanrayhan572@gmail.com>
2022-05-13 12:25:03 +0700

    ChitandaV03⁹

---
+++ README.md
@@ -0,0 +1,43 @@
+NOTE : SC JANGAN DIPERJUAL BELIKAN
+REUPLOAD ? BOLEH
+RECODE ? BOLEH
+JUAL ? MATI AJA LU SUU
+
+
+ //━━━━━━━━━━━━━━━[ ᴄᴏᴍᴍᴀɴᴅ ᴛᴇʀᴍᴜx ]━━━━━━━━━━━━━━━\\
+ $ pkg upgrade && pkg update
+ $ pkg install nodejs
+ $ pkg install yarn
+ $ pkg install git
+ $ pkg install libwebp
+ $ pkg install ffmpeg
+ $ pkg install imagemagick
+ $ termux-setup-storage
+ 
+ # RUN TERMUX BIASA
+ $ cd /sdcard
+ $ cd ChitandaV03
+ $ npm start
+
+ # RUN PM2
+ $ npm i pm2 -g
+ $ pm2 start index.js
+ $ pm2 save
+ $ pm2 monit 
+ //━━━━━━━━━━━━━━━[ ᴜᴘʟᴏᴀᴅ ᴛᴏ ʜᴇʀᴏᴋᴜ ]━━━━━━━━━━━━━━━\\
+ $ apt update
+ $ apt upgrade
+ $ pkg update
+ $ pkg upgrade
+ $ pkg install yarn
+ $ pkg install git
+ $ termux-setup-storage
+ $ cd /sdcard
+ $ cd chitanda-public
+ $ yarn add heroku
+ $ npm i -g heroku
+ 
+[ Next Command Cek Heroku ] 
+ 
+  //━━━━━━━━━━━━━━━[ ɴᴏᴛᴇ ]━━━━━━━━━━━━━━━\\
+ NOTE : Jika Mau Tanya Sesuatu Gausah Spam Jika Tidak Mau Di Block
---
+++ config.js
@@ -0,0 +1,78 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Web Apikey
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Tempat Apikey
+global.APIKeys = {
+	'https://zenzapis.xyz': 'YOUR_APIKEY', // Register Untuk Dapatkan Apikey
+}
+
+// Ganti Sewajarnya
+global.owner = ['62818617578']
+global.pengguna = 'Hanzz Genteng'
+global.premium = ['62818617578']
+global.pemilik = ['62818617578'] 
+global.ownernomer = '62818617578'
+global.ownername = 'Hamzz'
+global.botname = 'Hanzz - ＭＤ'
+global.Prefix = 'Multi'
+global.meki = '6285878313791'
+global.footer = 'Hanzz | © 2022'
+global.ig = 'https://instagram.com/'
+global.email = 'aliyanrayhan572@gmail.com'
+global.region = 'Indonesia'
+global.sc = 'https://m.youtube.com/'
+global.packname = '✨ Hanzz ✨' 
+global.author = 'Hanzz - ＭＤ' 
+global.sessionName = 'kizakixd'
+global.prefa = ['#','!','/','']
+global.sp = '» ' 
+global.mess = {
+    success: 'Done Your Request',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    error: 'Fitur sedang error!',
+        endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+    wrongFormat: 'Perintah Salah!!\nSertakan Link setelah Command..',
+    example1: 'Welcome @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @subject (Group Name)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)'
+,
+    example2: 'Good Bye @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @subject (Group Name)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)'    
+    
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 2,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./media/zaki.jpg')
+global.fakeImg = fs.readFileSync('./media/fake.jpg')
+
+//Ambil dr chika
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
